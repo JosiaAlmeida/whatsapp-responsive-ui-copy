fa9ec6065bbca4ba983a5ecdfdd09a88b187f5d8
Josia Almeida <josiaalmeida0001@gmail.com>
2022-02-09 19:31:52 +0100

    Construindo sistema responsivo

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:whatsappui/colors.dart';
+import 'package:whatsappui/responsive/responsive_layout.dart';
+import 'package:whatsappui/screens/mobile_screen_layout.dart';
+import 'package:whatsappui/screens/web_screen_layout.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Whatsapp UI',
+      theme: ThemeData.dark().copyWith(backgroundColor: backgroundColor),
+      home: ResponsiveLayout(
+          mobileScreenLayout: MobileScreenLayout(),
+          webScreenLayout: WebScreenLayout()),
+    );
+  }
+}
